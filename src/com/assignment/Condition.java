87136222d330e82d48bd956669041900686ce189
mayuri-nere <mayuri.nere96@gmail.com>
2022-09-18 14:44:33 +0530

    Mayuri's First Commit

---
+++ src/com/assignment/Condition.java
@@ -0,0 +1,12 @@
+package com.assignment;
+
+public class Condition {
+  public static void main(String[] ar) {
+	  int a = 65;
+		if (a >= 90 && a <= 100) {
+			System.out.println("Grade A");
+		} else if (a >= 75 && a <= 90) {
+			System.out.println("Grade B");
+		}
+  }
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Assignement2 {
+}
